3697b2b656f420337c4253b2c9b2a23aa4172cf0
kaito_ohishi <kaito990626@yahoo.o.jp+ohishikaito@users.noreply.github.com>
2020-09-18 12:42:43 +0900

    commit1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './common/navbar/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NavBarComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
